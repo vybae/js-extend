49f8d34269f36e725bdfa4815d65acf35174c7ec
junyalu <junyalu@outlook.com>
2019-03-22 01:43:59 +0800

    优化js浮点运算精度偏差的解决方法，尽量避免IEEE754标准不区分整数和浮点数带来的问题

--- float-cal-util.js
+++ float-cal-util.js
@@ -1,8 +1,11 @@
+/**
+ * Created by zhou_ on 2018-02-06.
+ */
 var floatCalUtil = function() {
     var util = {};
-     /**
+    /**
      * 拆解参数的整数部分和小数部分
-     * @param number {number}
+     * @param floatNumber
      * @returns {{nonRadixPointValue: number, negative: boolean, intPart: string, floatPart: string, float: number, int: number, digit: number}}
      */
     util.parseNumberBody = function (floatNumber) {
@@ -14,14 +17,17 @@ var floatCalUtil = function() {
 
         var s = /^(-?\d+)(\.\d+)?$/.exec(number),
             n = Number(s[1]),
-            fp = s[2]==undefined?"":s[2].substr(1);
+            fp = s[2]==undefined?"0":s[2],
+            fs = fp.substr(1);
         var ret = {
+            value: number,
             intPart: s[1],
-            floatPart: fp,
+            floatPart: fs,
             int: n,
-            float: Number((n>0?"":"-") + fp==""?"0":fp),
-            digit: fp.length,
-            negative: n > 0,
+            float: Number(number>=0?fp:"-"+fp),
+            floatToInt: Number(fs==""?"0":fs),
+            digit: fs.length,
+            negative: number > 0,
             nonRadixPointValue: Number(s[0].replace(/\./, ""))
         };
         return ret;
@@ -30,19 +36,30 @@ var floatCalUtil = function() {
     /**
      * 加法函数，用来得到精确的加法结果
      * 说明：javascript的加法结果会有误差，在两个浮点数相加的时候会比较明显。这个函数返回较为精确的加法结果。
-     * @param arg1 {number}
-     * @param arg2 {number}
+     * @param arg1
+     * @param arg2
      * @returns {number} arg1加上arg2的精确结果
      */
     util.add = function (arg1, arg2) {
         var a = util.parseNumberBody(arg1),
             b = util.parseNumberBody(arg2),
-            f = a.float + b.float,
-            fs = f.toString().replace(/^-/,""),
-            fc = a.float!=0 && b.float!=0
-                && fs.length > a.floatPart.length
-                && fs.length > b.floatPart.length;
-        var result = Number((a.int + b.int + (fc ? (f>0?1:-1) : 0)).toString() + "." + (fc ? fs.substr(1) : fs));
+            fl = Math.max(a.digit, b.digit),
+            aba = Math.abs(a.value),
+            abb = Math.abs(b.value),
+            symbolBit = aba > abb ? a.negative : b.negative, // 符号位
+            floatNegBit = a.float + b.float > 0, // 浮点正负位
+            borrowBit = false, // 浮点借位
+            fa = a.floatToInt * Math.pow(10, fl - a.digit),
+            fb = b.floatToInt * Math.pow(10, fl - b.digit),
+            f = a.negative == b.negative ? fa + fb : fa < fb && aba > abb
+                        ? (borrowBit = true, Math.pow(10, fl) + fa - fb) : fa > fb && aba < abb
+                        ? (borrowBit = true, Math.pow(10, fl) - fa + fb) : fa != 0 && fb != 0
+                        ? Math.abs(fa - fb) : Math.max(fa, fb),
+            fs = f.toString(),
+            overflowBit = a.negative == b.negative && fs.length > fl && f != 0, // 浮点溢出位
+            ft = Array(fl - fs.length + 1).join("0") + fs;
+        var result = (symbolBit?"":"-")+(Math.abs(a.int + b.int + (borrowBit || overflowBit ? (floatNegBit?1:-1) : 0))).toString() + "." + (overflowBit ? ft.substr(1) : ft);
+        result = Number(result); //.replace(/^-0(.0+)?$/, "0"))
         if (result < Number.MIN_SAFE_INTEGER || result > Number.MAX_SAFE_INTEGER) {
             throw "result is not precise";
         }
@@ -52,8 +69,8 @@ var floatCalUtil = function() {
     /**
      * 减法函数，用来得到精确的加法结果
      * 说明：javascript的减法结果会有误差，在两个浮点数相减的时候会比较明显。这个函数返回较为精确的加法结果。
-     * @param arg1 {number}
-     * @param arg2 {number}
+     * @param arg1
+     * @param arg2
      * @returns {number} arg1减去arg2的精确结果
      */
     util.sub = function (arg1, arg2) {
@@ -82,8 +99,8 @@ var floatCalUtil = function() {
     /**
      *  除法函数，用来得到精确的除法结果
      *  javascript的除法结果会有误差，在两个浮点数相除的时候会比较明显。这个函数返回较为精确的除法结果。
-     * @param arg1 被除数
-     * @param arg2 除数
+     * @param arg1
+     * @param arg2
      * @returns {number} arg1除以arg2的精确结果
      */
     util.div = function (arg1, arg2) {
@@ -103,12 +120,13 @@ var floatCalUtil = function() {
     /**
      * 四舍五入
      * 重写toFixed，浏览器之间遇5进位判断混乱，部分浏览器下toFixed遇5不进位(比如2.55 toFixed(1) 为2.5)，部分遇5进位
-     * @param number 被格式化的浮点数
+     * @param floatNumber 被格式化的浮点数
      * @param retainDigits 保留小数的位数，参数默认值为0，必须为正整数或不传
      * @param meet5carry 是否遇5进位的逻辑，参数默认值为true，必须为布尔值或不传
      * @returns {number}
      */
-    util.toFixed = function(number, retainDigits, meet5carry) {
+    util.toFixed = function(floatNumber, retainDigits, meet5carry) {
+        var number = Number(floatNumber);
         if (typeof number != "number" || parseFloat(number).toString() == "NaN")
             throw "argument must be number";
         if ((typeof retainDigits != "number" || !/^(?!0)\d+$/.test(retainDigits) || parseFloat(retainDigits).toString() == "NaN") && undefined!==retainDigits)
@@ -126,28 +144,23 @@ var floatCalUtil = function() {
                 return (Number(pattern1+pattern2) + (Number(pattern3)>4&&(meet5carry||Number(pattern3)>5) ? 1 : 0)) / Math.pow(10,pattern2.length);
             }));
         } else return number;
-    }
-    
+    };
+
     return util;
 }();
 
-// 加
 Number.prototype.add = function (arg) {
     return floatCalUtil.add(this, arg);
 };
-// 减
 Number.prototype.sub = function (arg) {
     return floatCalUtil.sub(this, arg);
 };
-// 乘
 Number.prototype.mul = function (arg) {
     return floatCalUtil.mul(this, arg);
 };
-// 除
 Number.prototype.div = function (arg) {
     return floatCalUtil.div(this, arg);
 };
-// 小数精确保留位数，四舍五入
 Number.prototype.toFixed = function (retainDigits, meet5carry) {
     return floatCalUtil.toFixed(this, retainDigits, meet5carry);
 };
